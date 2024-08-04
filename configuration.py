0e2afdb9203d54f569a1d4438685919c2ff09567
alondravi <alondravillagrana@hotmail.com>
2024-08-03 18:57:09 -0600

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-0a52e275-988a-4b42-b236-3cd8ff7da53c.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-c0b07486-de60-4b34-96f1-049ec12468bb.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
