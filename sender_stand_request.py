3e32619e1a7872352bb3bde574da91c17396f7f2
alondravi <alondravillagrana@hotmail.com>
2024-08-03 18:05:21 -0600

    sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,15 +9,6 @@ def post_new_user(body):
                          headers=data.headers)
 
 
-response = post_new_user(data.user_body)
-print(response.status_code)
-print(response.json())
-
-assert response.status_code == 201
-assert response.json()["authToken"] != ""
-
-auth_token = response.json()["authToken"]
-
 def post_new_client_kit(kit_body, auth_token):
     headers = {
         "Authorization": f"Bearer {auth_token}",
@@ -28,11 +19,3 @@ def post_new_client_kit(kit_body, auth_token):
                          headers=headers)
 
 
-kit_body = data.kit_body.copy()
-response = post_new_client_kit(kit_body, auth_token)
-
-print(response.status_code)
-print(response.json())
-
-assert response.status_code == 201
-assert kit_body["name"] == response.json()["name"]
